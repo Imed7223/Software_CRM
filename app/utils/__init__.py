d56ec7e4e7f77f6f199a6d0b44f6daf336004e23
IMED-EDDINE MIHOUBI <mihoubiimededdine@gmail.com>
2025-12-20 23:21:47 +0100

    utils/__init__.py

---
+++ app/utils/__init__.py
@@ -0,0 +1,43 @@
+from .logging_config import setup_logging, log_info, log_error, log_warning, log_debug
+from .auth import (
+    authenticate_user,
+    verify_password,
+    hash_password,
+    get_user_permissions,
+    has_permission,
+    require_permission
+)
+from .validators import (
+    validate_email,
+    validate_phone,
+    validate_date,
+    validate_datetime,
+    validate_amount,
+    validate_integer,
+    clean_phone_number,
+    format_phone_number,
+    validate_department
+)
+
+__all__ = [
+    'setup_logging',
+    'log_info',
+    'log_error',
+    'log_warning',
+    'log_debug',
+    'authenticate_user',
+    'verify_password',
+    'hash_password',
+    'get_user_permissions',
+    'has_permission',
+    'require_permission',
+    'validate_email',
+    'validate_phone',
+    'validate_date',
+    'validate_datetime',
+    'validate_amount',
+    'validate_integer',
+    'clean_phone_number',
+    'format_phone_number',
+    'validate_department'
+]
