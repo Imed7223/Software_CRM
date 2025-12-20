0cdd42f573af7a1fb5206ee3ca73b5e55953f6a3
IMED-EDDINE MIHOUBI <mihoubiimededdine@gmail.com>
2025-12-20 23:47:01 +0100

    menus/__init__.py

---
+++ app/menus/__init__.py
@@ -0,0 +1,7 @@
+from .main_menu import main_menu
+from .users_menu import menu_users
+from .clients_menu import menu_clients
+from .contracts_menu import menu_contracts
+from .events_menu import menu_events
+
+__all__ = ['main_menu', 'menu_users', 'menu_clients', 'menu_contracts', 'menu_events']
