a6b7b92f3ecc1752b38177e56bc5352e7c13c4a3
IMED-EDDINE MIHOUBI <mihoubiimededdine@gmail.com>
2025-12-20 22:18:05 +0100

    models/__init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User, Department
+from .client import Client
+from .contract import Contract
+from .event import Event
+
+__all__ = ['User', 'Department', 'Client', 'Contract', 'Event']
